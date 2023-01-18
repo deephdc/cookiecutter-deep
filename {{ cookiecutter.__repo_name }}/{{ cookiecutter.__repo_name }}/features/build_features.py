40cf9f9e16333c13b900ca2c9f15119a1e49714a
valentin.kozlov <valentin.kozlov@kit.edu>
2023-01-19 00:50:50 +0100

    [advanced] replace repo_name with __repo_name

---
+++ {{ cookiecutter.__repo_name }}/{{ cookiecutter.__repo_name }}/features/build_features.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Feature building
+"""
+
+# import project config.py
+import {{ cookiecutter.__repo_name }}.config as cfg
---
+++ {{ cookiecutter.__repo_name }}/{{ cookiecutter.__repo_name }}/visualization/visualize.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Visualization
+"""
+# import project config.py
+import {{ cookiecutter.__repo_name }}.config as cfg
