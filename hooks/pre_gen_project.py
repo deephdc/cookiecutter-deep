e95847c4bd4e71894aefb8d0e3098b36c6e6664b
valentin.kozlov <valentin.kozlov@kit.edu>
2023-03-28 19:15:01 +0200

    [hooks] Add pre_gen_project.py hook to check repo_name validity  Changes to be committed: 	new file:   pre_gen_project.py

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2018 - 2023 Karlsruhe Institute of Technology - Steinbuch Centre for Computing
+# This code is distributed under the MIT License
+# Please, see the LICENSE file
+
+"""
+    Pre-hook script
+    Checks that {{ cookiecutter.__repo_name }}:
+    1. is not too short
+    2. has valid characters
+"""
+
+import re
+
+MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+
+repo_name = '{{ cookiecutter.__repo_name }}'
+
+if (not re.match(MODULE_REGEX, repo_name) or
+    len(repo_name) < 2):
+    raise ValueError('%s is not a valid Python module name!\n\
+Please, check the \"project_name\" input' % repo_name)
+
